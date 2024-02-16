ac7df526eb4cda84c74caabc8afe239dcb1f0066
DAM <saul.mancas@educa.jcyl.es>
2024-02-16 09:38:18 +0100

    Clase DAM

---
+++ src/DAM.java
@@ -0,0 +1,22 @@
+public class DAM {
+    private int macacos;
+
+    public void suspensos(){
+
+    }
+
+    public int getMacacos() {
+        return macacos;
+    }
+
+    public void setMacacos(int macacos) {
+        this.macacos = macacos;
+    }
+
+    @Override
+    public String toString() {
+        return "DAM{" +
+                "macacos=" + macacos +
+                '}';
+    }
+}
